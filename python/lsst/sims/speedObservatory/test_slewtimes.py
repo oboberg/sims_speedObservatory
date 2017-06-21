781fd68e28c90f2c033e52525f44f8507784d7e0
Daniel R <drothchild@college.harvard.edu>
2017-06-21 01:20:54 -0400

    Built out test_slewtimes.py
    It now makes three plots/sets of plots:
    1. It generates random slews in altitude and azimuth and computes SOCS
    and speedObservatory slew times, plotting the difference.
    2. It calculates SOCS and speedObservatory slew times over a grid of
    azimuths, holding altitude constant. It plots the slew times as
    calculated by each method and also the difference.
    3. Same as 2 but over a grid of altitudes, holding azimuth constant.
    
    As of this commit, SOCS and speedObservatory produce identical slew
    times in all 3 tests (except for altitudes very close to zenith for
    some reason)

--- python/lsst/sims/speedObservatory/test_slewtimes.py
+++ python/lsst/sims/speedObservatory/test_slewtimes.py
@@ -3,6 +3,7 @@ import healpy as hp
 import copy
 from lsst.sims.ocs.observatory import MainObservatory
 from lsst.sims.ocs.configuration import ObservingSite, Observatory
+from telescope import Telescope
 # Generate a few matrices to make it easy to generate slewtime maps
 
 from matplotlib import pyplot as plt
@@ -31,7 +32,7 @@ def opsimSlewTime(model, alt1=None, alt2=None, az1=None, az2=None, rot1=None, ro
                      model.current_state.filter)
 
     final_slew_state = copy.deepcopy(model.current_state)
-    slew_time = (final_slew_state.time - initial_slew_state.time, "seconds")
+    slew_time = final_slew_state.time - initial_slew_state.time
 
     return slew_time
 
@@ -42,35 +43,102 @@ mo.configure(Observatory())
 model = mo.model
 
 # initialize alt-sched observatory
-speedObs = SpeedObservatory()
+tel = Telescope()
 
+numRand = 10000
+randMax = np.radians(30)
+deltaAlts = np.random.rand(numRand) * np.sqrt(randMax)
+deltaAzes = np.random.rand(numRand) * np.sqrt(randMax)
+opsim_slew_times = []
+fast_slew_times = []
+for deltaAlt, deltaAz in zip(deltaAlts, deltaAzes):
+    # make altitude start at 45 degs so opsim doesn't complain
+    # if we ask it to point at the ground
+    opsim_slew_time = opsimSlewTime(model, alt1=45,
+                                    alt2=np.degrees(deltaAlt)+45,
+                                    az1=0,
+                                    az2=np.degrees(deltaAz))
+    fast_slew_time = tel.calcSlewTime(0, 0, 'u',
+                                      deltaAlt, deltaAz, 'u')
+    opsim_slew_times.append(opsim_slew_time)
+    fast_slew_times.append(fast_slew_time)
+opsim_slew_times = np.array(opsim_slew_times)
+fast_slew_times = np.array(fast_slew_times)
+plt.scatter(fast_slew_times, fast_slew_times - opsim_slew_times)
+plt.title("Error for random slews")
+plt.xlabel("Slew time (seconds)")
+plt.ylabel("Error (fast - opsim) (seconds)")
 
 # calculate slew times using both methods over alt and az grids
-delta = 5
 
-azimuths = np.arange(0, 360+delta, delta)
-altitudes = np.arange(20., 90.+delta, delta)
+# we'll consider slews between az=azMin...azMax; and alt=altMin..altMax
+# at a resolution of delta
+azMin  = np.radians(0)
+azMax  = np.radians(90) # set to 90 instead of 360 to save time
+altMin = np.radians(20)
+altMax = np.radians(80)
+delta  = np.radians(0.5)
 
+azimuths = np.arange(azMin, azMax+delta, delta)
+altitudes = np.arange(altMin, altMax+delta, delta)
+
+# opsim_alt_array, for example, will hold the time, according to opsimSlewTime,
+# that it takes to slew between every altitudes.size^2 combindation of altitudes
+# while keeping the azimuth constant. The other 3 arrays are similar
 opsim_alt_array = np.zeros((altitudes.size, altitudes.size), dtype=float)
-opsim_az_array  = np.zeros((azimuths.size,  azimuths.size), dtype=float)
+opsim_az_array  = np.zeros((azimuths.size,  azimuths.size),  dtype=float)
 
 fast_alt_array = np.zeros((altitudes.size, altitudes.size), dtype=float)
-fast_az_array  = np.zeros((azimuths.size,  azimuths.size), dtype=float)
+fast_az_array  = np.zeros((azimuths.size,  azimuths.size),  dtype=float)
 
+# populate the opsim and fast azimuth arrays
 for i in range(azimuths.size):
     for j in range(azimuths.size):
-        opsim_slew_time = opsimSlewTime(model, az1=azimuths[i], az2=azimuths[j])
-        opsim_az_array[i, j] += opsim_slew_time[0]
+        opsim_slew_time = opsimSlewTime(model, az1=np.degrees(azimuths[i]),
+                                               az2=np.degrees(azimuths[j]))
+        opsim_az_array[i, j] = opsim_slew_time
 
-        fast_slew_time = speedObs.calcSlewTime(np.array([0,az1]), 'u', 
-                                               np.array([0,az2]), 'u')
-        fast_az_array[i, j] += fast_slew_time
+        fast_slew_time = tel.calcSlewTime(0, azimuths[i], 'u',
+                                          0, azimuths[j], 'u')
+        fast_az_array[i, j] = fast_slew_time
 
-plt.imshow(opsim_az_array, interpolation='none', extent=[0,360,0,360])
-plt.imshow(alt_az_array,   interpolation='none', extent=[0,360,0,360])
-plt.show()
-
+# and populate the opsim and fast altitude arrays
 for i in range(altitudes.size):
     for j in range(altitudes.size):
-        st = slewtime(model, alt1=altitudes[i], alt2=altitudes[j])
-        alt_array[i, j] += st[0]
+        opsim_slew_time = opsimSlewTime(model, alt1=np.degrees(altitudes[i]),
+                                               alt2=np.degrees(altitudes[j]))
+        opsim_alt_array[i, j] = opsim_slew_time
+
+        fast_slew_time = tel.calcSlewTime(altitudes[i], 0, 'u',
+                                          altitudes[j], 0, 'u')
+        fast_alt_array[i, j] = fast_slew_time
+
+# convenience function for plotting the arrays
+def plot(arr, minRad, maxRad, title="", xlabel="", ylabel="", clim=None):
+    plt.figure(title)
+    plt.title(title)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.imshow(arr[::-1,:], interpolation='none',
+               extent=np.degrees([minRad,maxRad,minRad,maxRad]),
+               clim=clim)
+    plt.colorbar()
+
+# plot opsim, fast, and the difference for alt and az arrays
+plot(opsim_az_array, azMin, azMax, "SOCS Azimuth Slew Times",
+     "Final azimuth (deg)", "Initial azimuth (deg)")
+plot(fast_az_array, azMin, azMax, "speedObservatory Azimuth Slew Times",
+     "Final azimuth (deg)", "Initial azimuth (deg)")
+plot(fast_az_array - opsim_az_array, azMin, azMax,
+     "Azimuth Slew Time Error (speedObservatory - SOCS)",
+     "Final azimuth (deg)", "Initial azimuth (deg)", clim=(-0.01, 0.01))
+
+plot(opsim_alt_array, altMin, altMax, "SOCS Altitude Slew Times",
+     "Final altitude (deg)", "Initial altitude (deg)")
+plot(fast_alt_array, altMin, altMax, "speedObservatory Altitude Slew Times",
+     "Final altitude (deg)", "Initial altitude (deg)")
+plot(fast_alt_array - opsim_alt_array, altMin, altMax,
+     "Altitude Slew Time Error (speedObservatory - SOCS)",
+     "Final altitude (deg)", "Initial altitude (deg)", clim=(-0.01, 0.01))
+
+plt.show()
