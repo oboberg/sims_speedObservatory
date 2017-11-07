e145bcff36b292a0f8bb6a9841e7215eadb2f1ac
Tiago Ribeiro <tiago.astro@gmail.com>
2017-11-07 09:20:45 -0700

    Make it transparent to the module whether it uses sims.ocs or not. If sims.ocs is present it will work the same way it did before, no changes are required. If no sims.ocs module is found it logs the exception and issues a warning that the user must provide the required modules.

---
+++ python/lsst/sims/speedObservatory/model_notime.py
@@ -0,0 +1,35 @@
+from lsst.sims.ocs.environment import SeeingModel, CloudModel
+
+class SeeingModel_no_time(SeeingModel):
+    """Eliminate the need to use a time_handler object
+    """
+    def __init__(self, offset=0.):
+        """
+        Parameters
+        ----------
+        offset : float
+            XXX-I don't even know the units on this. Days maybe?
+        """
+        self.seeing_db = None
+        self.seeing_dates = None
+        self.seeing_values = None
+        self.environment_config = None
+        self.filters_config = None
+        self.seeing_fwhm_system_zenith = None
+        self.offset = offset
+
+
+class CloudModel_no_time(CloudModel):
+    """Eliminate the need to use a time_handler object
+    """
+    def __init__(self, offset=0.):
+        """Initialize the class.
+
+        Parameters
+        ----------
+        offset : float (0.)
+        """
+        self.cloud_db = None
+        self.cloud_dates = None
+        self.cloud_values = None
+        self.offset = offset
